0598531c83f07fc078b86126e16b0f9171dcc326
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-09 23:32:24 -0500

    Added the webpack config file.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './public/app.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module : {
+		loaders :[
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader', 
+				query : {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+};
