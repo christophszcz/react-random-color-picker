805fbfe7bf611f0ad9b14558396c653682d76931
christophszcz <christoph.szczechowicz@gmail.com>
2017-03-09 23:42:43 -0500

    Input component template code.

---
+++ public/components/Button.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Button = React.createClass({
+	render: function () {
+		return (
+			<button 
+				className={ this.props.light ? 'light-button' : 'dark-button' }>
+				Refresh
+			</button>
+		);
+	}
+});
