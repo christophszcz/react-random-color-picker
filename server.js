5c3d6e5525922aeeeba4aa6138665ba4cbd57da7
christophszcz <christoph.szczechowicz@gmail.com>
2017-05-10 15:17:36 -0400

    Input heroku deployment scripts.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var express = require('express');
+// var express = require('express');
 
 //Create App
 
-var app = express();
-app.use(express.static('public'));
+// var app = express();
+// app.use(express.static('public'));
 
-app.listen(3000, function() {
-	console.log('Express server is running on port 3000.');
+// app.listen(3000, function() {
+// 	console.log('Express server is running on port 3000.');
+// });
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.listen(port);
+console.log('Server started');
